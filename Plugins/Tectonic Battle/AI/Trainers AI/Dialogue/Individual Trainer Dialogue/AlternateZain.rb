38c5d3d2cb6783b722c5980849f1913d2f2fa5df
ZinniaGameDev <xeuorux@gmail.com>
2026-01-08 18:42:08 -0700

    Scrub in-battle dialogue

--- Plugins/Tectonic Battle/AI/Trainers AI/Dialogue/Individual Trainer Dialogue/AlternateZain.rb
+++
@@ -1,9 +0,0 @@
-PokeBattle_AI::TrainerSendsOutPokemonDialogue.add(:ALTERNATE_ZAIN,
-  proc { |_policy, battler, trainer_speaking, dialogue_array|
-      if battler.species == :GIRATINA && !trainer_speaking.policyStates[:GiratinaComment]
-          dialogue_array.push(_INTL("Yes, my lord. The battle is ready for your presence."))
-          trainer_speaking.policyStates[:GiratinaComment] = true
-      end
-      next dialogue_array
-  }
-)
